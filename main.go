e3068196cbb84b854561f057368f1c5ad851a4ef
Pramesh Karki <parameswarkarki@gmail.com>
2023-11-03 08:10:31 +0545

    refactor: removed unused code

--- main.go
+++ main.go
@@ -1,8 +1,4 @@
 package main
 
-import nepali_utils "github.com/PrameshKarki/nepali_utils/lib"
-
 func main() {
-	nepali,_:=nepali_utils.EnglishToNepali("Mero Nepal") // Result: मेरो नेपाल
-	currencyInWord:=nepali_utils.NumberToNepaliWord(12345) // Result: बाह्र हजार तीन सय पैंतालिस
 }
